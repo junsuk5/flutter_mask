d27003ddde55a00155693aebaed0bd2c575cd870
Junsuk Oh <a811219@gmail.com>
2020-04-02 21:27:48 +0900

    1부 일단 만들어 보는 마스크 재고 앱

---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mask/model/store.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final stores = List<Store>();
+  var isLoading = true;
+
+  Future fetch() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    var url =
+        'https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat=37.266389&lng=126.999333&m=5000';
+
+    var response = await http.get(url);
+
+    final jsonResult = jsonDecode(utf8.decode(response.bodyBytes));
+
+    final jsonStores = jsonResult['stores'];
+
+    setState(() {
+      stores.clear();
+      jsonStores.forEach((e) {
+        stores.add(Store.fromJson(e));
+      });
+      isLoading = false;
+    });
+    print('fetch완료');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetch();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('마스크 재고 있는 곳 : ${stores.where((e) {
+          return e.remainStat == 'plenty' ||
+              e.remainStat == 'some' ||
+              e.remainStat == 'few';
+        }).length}곳'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: fetch,
+          )
+        ],
+      ),
+      body: isLoading == true
+          ? loadingWidget()
+          : ListView(
+              children: stores.where((e) {
+                return e.remainStat == 'plenty' ||
+                    e.remainStat == 'some' ||
+                    e.remainStat == 'few';
+              }).map((e) {
+                return ListTile(
+                  title: Text(e.name),
+                  subtitle: Text(e.addr),
+                  trailing: _buildRemainStatWidget(e),
+                );
+              }).toList(),
+            ),
+    );
+  }
+
+  Widget _buildRemainStatWidget(Store store) {
+    var remainStat = '판매중지';
+    var description = '판매중지';
+    var color = Colors.black;
+    if (store.remainStat == 'plenty') {
+      remainStat = '충분';
+      description = '100개 이상';
+      color = Colors.green;
+    }
+    switch (store.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+      case 'some':
+        remainStat = '보통';
+        description = '30 ~ 100개';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        remainStat = '부족';
+        description = '2 ~ 30개';
+        color = Colors.red;
+        break;
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.grey;
+        break;
+      default:
+    }
+
+    return Column(
+      children: <Widget>[
+        Text(
+          remainStat,
+          style: TextStyle(color: color, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          description,
+          style: TextStyle(color: color),
+        ),
+      ],
+    );
+  }
+
+  Widget loadingWidget() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('정보를 가져오는 중'),
+          CircularProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/model/store.dart
@@ -0,0 +1,48 @@
+class Store {
+  String code;
+  String name;
+  String addr;
+  String type;
+  num lat;
+  num lng;
+  String stockAt;
+  String remainStat;
+  String createdAt;
+
+  Store(
+      {this.code,
+        this.name,
+        this.addr,
+        this.type,
+        this.lat,
+        this.lng,
+        this.stockAt,
+        this.remainStat,
+        this.createdAt});
+
+  Store.fromJson(Map<String, dynamic> json) {
+    code = json['code'];
+    name = json['name'];
+    addr = json['addr'];
+    type = json['type'];
+    lat = json['lat'];
+    lng = json['lng'];
+    stockAt = json['stock_at'];
+    remainStat = json['remain_stat'];
+    createdAt = json['created_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['code'] = this.code;
+    data['name'] = this.name;
+    data['addr'] = this.addr;
+    data['type'] = this.type;
+    data['lat'] = this.lat;
+    data['lng'] = this.lng;
+    data['stock_at'] = this.stockAt;
+    data['remain_stat'] = this.remainStat;
+    data['created_at'] = this.createdAt;
+    return data;
+  }
+}
